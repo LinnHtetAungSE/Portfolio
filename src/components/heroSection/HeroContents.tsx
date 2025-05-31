293cee005aa9b8310a84a31a2eccc1c9864397ba
Linn Htet Aung <linnhtetaung@Linns-MacBook-Air.local>
2025-05-31 11:44:31 +0700

    [modify] update resume

--- src/components/heroSection/HeroContents.tsx
+++ src/components/heroSection/HeroContents.tsx
@@ -12,7 +12,7 @@ export const HeroContent: React.FC = () => {
       </p>
       <Button className="w-fit flex items-center gap-x-3 text-sm sm:text-base font-semibold group">
         <a
-          href="https://drive.google.com/uc?export=download&id=1hnWo5JSpLm9UrsCFKpSr86a0m3ehPrtO"
+          href="https://drive.google.com/uc?export=download&id=1sND1sDvMsr_hjFYSOQh1KR9kWevlingV"
           target="_blank"
           rel="noopener noreferrer"
           className="flex items-center gap-x-3"
